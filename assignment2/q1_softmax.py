b6c3d80ef10723e5a12bf54ce46da4390ac2f8a4
Yue Xiang <yx1107@foxmail.com>
2018-10-25 08:58:07 +0800

    yx: q1 init

--- assignment2/q1_softmax.py
+++ assignment2/q1_softmax.py
@@ -1,90 +1,102 @@
 import numpy as np
+import tensorflow as tf
+from utils.general_utils import test_all_close
 
 
 def softmax(x):
-    """Compute the softmax function for each row of the input x.
-
-    It is crucial that this function is optimized for speed because
-    it will be used frequently in later code. You might find numpy
-    functions np.exp, np.sum, np.reshape, np.max, and numpy
-    broadcasting useful for this task.
-
-    Numpy broadcasting documentation:
-    http://docs.scipy.org/doc/numpy/user/basics.broadcasting.html
-
-    You should also make sure that your code works for a single
-    N-dimensional vector (treat the vector as a single row) and
-    for M x N matrices. This may be useful for testing later. Also,
-    make sure that the dimensions of the output match the input.
-
-    You must implement the optimization in problem 1(a) of the
-    written assignment!
-
-    Arguments:
-    x -- A N dimensional vector or M x N dimensional numpy matrix.
-
-    Return:
-    x -- You are allowed to modify x in-place
     """
-    orig_shape = x.shape
+    Compute the softmax function in tensorflow.
 
-    if len(x.shape) > 1:
-        # Matrix
-        ### YOUR CODE HERE
-        x = (x.T - np.max(x, axis=1)).T
-        x = (np.exp(x).T / np.sum(np.exp(x), axis=1)).T
-        #raise NotImplementedError
-        ### END YOUR CODE
-    else:
-        # Vector
-        ### YOUR CODE HERE
-        x = x - np.max(x)
-        x =  np.exp(x) / np.sum(np.exp(x))
-        #raise NotImplementedError
-        ### END YOUR CODE
-    assert x.shape == orig_shape
-    return x
+    You might find the tensorflow functions tf.exp, tf.reduce_max,
+    tf.reduce_sum, tf.expand_dims useful. (Many solutions are possible, so you may
+    not need to use all of these functions). Recall also that many common
+    tensorflow operations are sugared (e.g. x * y does a tensor multiplication
+    if x and y are both tensors). Make sure to implement the numerical stability
+    fixes as in the previous homework!
+
+    Args:
+        x:   tf.Tensor with shape (n_samples, n_features). Note feature vectors are
+                  represented by row-vectors. (For simplicity, no need to handle 1-d
+                  input as in the previous homework)
+    Returns:
+        out: tf.Tensor with shape (n_sample, n_features). You need to construct this
+                  tensor in this problem.
+    """
+
+    ### YOUR CODE HERE
+    ### END YOUR CODE
+
+    return out
+
+
+def cross_entropy_loss(y, yhat):
+    """
+    Compute the cross entropy loss in tensorflow.
+    The loss should be summed over the current minibatch.
+
+    y is a one-hot tensor of shape (n_samples, n_classes) and yhat is a tensor
+    of shape (n_samples, n_classes). y should be of dtype tf.int32, and yhat should
+    be of dtype tf.float32.
+
+    The functions tf.to_float, tf.reduce_sum, and tf.log might prove useful. (Many
+    solutions are possible, so you may not need to use all of these functions).
+
+    Note: You are NOT allowed to use the tensorflow built-in cross-entropy
+                functions.
+
+    Args:
+        y:    tf.Tensor with shape (n_samples, n_classes). One-hot encoded.
+        yhat: tf.Tensorwith shape (n_sample, n_classes). Each row encodes a
+                    probability distribution and should sum to 1.
+    Returns:
+        out:  tf.Tensor with shape (1,) (Scalar output). You need to construct this
+                    tensor in the problem.
+    """
+
+    ### YOUR CODE HERE
+    ### END YOUR CODE
+
+    return out
 
 
 def test_softmax_basic():
     """
-    Some simple tests to get you started.
+    Some simple tests of softmax to get you started.
     Warning: these are not exhaustive.
     """
-    print "Running basic tests..."
-    test1 = softmax(np.array([1,2]))
-    print test1
-    ans1 = np.array([0.26894142,  0.73105858])
-    assert np.allclose(test1, ans1, rtol=1e-05, atol=1e-06)
 
-    test2 = softmax(np.array([[1001,1002],[3,4]]))
-    print test2
-    ans2 = np.array([
-        [0.26894142, 0.73105858],
-        [0.26894142, 0.73105858]])
-    assert np.allclose(test2, ans2, rtol=1e-05, atol=1e-06)
+    test1 = softmax(tf.constant(np.array([[1001, 1002], [3, 4]]), dtype=tf.float32))
+    with tf.Session() as sess:
+            test1 = sess.run(test1)
+    test_all_close("Softmax test 1", test1, np.array([[0.26894142,  0.73105858],
+                                                      [0.26894142,  0.73105858]]))
 
-    test3 = softmax(np.array([[-1001,-1002]]))
-    print test3
-    ans3 = np.array([0.73105858, 0.26894142])
-    assert np.allclose(test3, ans3, rtol=1e-05, atol=1e-06)
+    test2 = softmax(tf.constant(np.array([[-1001, -1002]]), dtype=tf.float32))
+    with tf.Session() as sess:
+            test2 = sess.run(test2)
+    test_all_close("Softmax test 2", test2, np.array([[0.73105858, 0.26894142]]))
 
-    print "You should be able to verify these results by hand!\n"
+    print "Basic (non-exhaustive) softmax tests pass\n"
 
 
-def test_softmax():
+def test_cross_entropy_loss_basic():
     """
-    Use this space to test your softmax implementation by running:
-        python q1_softmax.py
-    This function will not be called by the autograder, nor will
-    your tests be graded.
+    Some simple tests of cross_entropy_loss to get you started.
+    Warning: these are not exhaustive.
     """
-    print "Running your tests..."
-    ### YOUR CODE HERE
-    #raise NotImplementedError
-    ### END YOUR CODE
+    y = np.array([[0, 1], [1, 0], [1, 0]])
+    yhat = np.array([[.5, .5], [.5, .5], [.5, .5]])
 
+    test1 = cross_entropy_loss(
+            tf.constant(y, dtype=tf.int32),
+            tf.constant(yhat, dtype=tf.float32))
+    with tf.Session() as sess:
+        test1 = sess.run(test1)
+    expected = -3 * np.log(.5)
+    test_all_close("Cross-entropy test 1", test1, expected)
+
+    print "Basic (non-exhaustive) cross-entropy tests pass"
 
 if __name__ == "__main__":
     test_softmax_basic()
-    test_softmax()
+    test_cross_entropy_loss_basic()
